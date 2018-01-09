dd425a982e05b1acef4b5147c355b7c57923180a
acer <icelnw135@gmail.com>
2018-01-09 15:44:29 +0700

    Back Controler

--- app/src/main/java/chaimonkol/krirk/ac/th/icecalculate/fragment/CalculateFragment.java
+++ app/src/main/java/chaimonkol/krirk/ac/th/icecalculate/fragment/CalculateFragment.java
@@ -3,10 +3,12 @@ package chaimonkol.krirk.ac.th.icecalculate.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import chaimonkol.krirk.ac.th.icecalculate.MainActivity;
 import chaimonkol.krirk.ac.th.icecalculate.R;
 
 /**
@@ -15,6 +17,37 @@ import chaimonkol.krirk.ac.th.icecalculate.R;
 
 public class CalculateFragment extends Fragment{
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+
+        createToolbar();
+
+    } // Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolberCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle("Calculate");
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
+
 
     @Nullable
     @Override
@@ -22,4 +55,6 @@ public class CalculateFragment extends Fragment{
         View view = inflater.inflate(R.layout.fragment_calculate, container, false);
         return view;
     }
+
+
 }  //Main class
